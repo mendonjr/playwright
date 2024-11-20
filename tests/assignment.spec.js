cd472df490ff4cf836e965c7be9a9a8419d6b0c1
Prasad Mendon <prasadmendon744@gmail.com>
2024-11-20 18:26:37 +0530

    1. searching with text-only 2.searching with tagname and text locator strategy 3. Adding assertions with synchronization steps like waitFor() 4. Changes for Handling auto suggestive dropdowns

--- tests/assignment.spec.js
+++ tests/assignment.spec.js
@@ -1,6 +1,6 @@
 const {test,expect} = require('@playwright/test');
 
-test.only('Shopping Web App Demo', async({page})=>{
+test.only('Shopping Web App E2E scenario', async({page})=>{
     await page.goto('https://rahulshettyacademy.com/client');
     await page.locator('#userEmail').fill('prasad108@gmail.com');
     await page.locator('#userPassword').fill('Hare@krishna108');
@@ -19,13 +19,41 @@ test.only('Shopping Web App Demo', async({page})=>{
 
     const products = await page.locator('.card-body');
     const count = await products.count();
-    await page.pause()
+    
     for(let i=0;i<count;i++){
         if(await products.nth(i).locator('b').textContent() === 'IPHONE 13 PRO'){
-            //add to cart
-            await products.nth(i).locator("text= Add To Cart").click();
+            //add to cart by searching element with text-only
+            await products.nth(i).locator("text= Add to Cart").click();  //searching the element based on text only once initial search is done
+            // await products.nth(i).locator("//button[contains(text(),' Add To Cart')]").click();
             break;
         }
     }
+    await page.locator(`[routerlink*='cart']`).click();
+    //wait for an element as AUTO - WAITING feature is not enabled for isVisible method
+    await page.locator('button:has-text("Buy")').waitFor();
+    //psuedo class usage with text for searching element
+    const bool = await page.locator('h3:has-text("IPHONE 13 PRO")').isVisible();
+
+    expect(bool).toBeTruthy();
+
+    await page.locator('button:has-text("Checkout")').click();
+    await page.locator('[placeholder*="Country"]').pressSequentially('Ind');
     
+    //selecting dropdown value after checking desired option exists
+    await page.locator('.ta-results').waitFor();
+    const dropdown =  page.locator('.ta-item');
+    const optionsCount = await dropdown.count();
+
+    for(let i =0; i<optionsCount;i++){
+
+        const text = await dropdown.nth(i).textContent();
+
+        if(text.trim() === 'India'){
+            dropdown.nth(i).click();
+            break;
+        }
+    }
+
+    await page.pause();
+
 })
